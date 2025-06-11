489c584ecb401e5bc48d2b1a6acb144b56e6216b
Denver <audgkwns44@gmail.com>
2025-06-11 16:15:32 +0900

    [Update]announcement fonts theming

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,24 @@
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface TypographyVariants {
+    announcePreviewTitle: React.CSSProperties;
+    announcePreviewContent: React.CSSProperties;
+  }
+
+  interface TypographyVariantsOptions {
+    announcePreviewTitle?: React.CSSProperties;
+    announcePreviewContent: React.CSSProperties;
+  }
+}
+
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    announcePreviewTitle: true;
+    announcePreviewContent: true;
+  }
+}
+
 const theme = createTheme({
   typography: {
     body1: {
@@ -7,11 +26,29 @@ const theme = createTheme({
       fontWeight: 600,
       fontFamily: "Pretendard, sans-serif",
     },
-    // 필요하면 다른 variant도 설정 가능
     subtitle1: {
       fontSize: "18px",
       fontWeight: 700,
       fontFamily: "Pretendard, sans-serif",
+    },
+    announcePreviewTitle: {
+      fontSize: "16px",
+      fontWeight: 800,
+      fontFamily: "Pretendard, sans-serif",
+    },
+    announcePreviewContent: {
+      fontSize: "14px",
+      fontWeight: 400,
+      fontFamily: "Pretendard, sans-serif",
+      color: "gray",
+      textAlign: "left",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      WebkitLineClamp: 2,
+      WebkitBoxOrient: "vertical",
+      lineClamp: 2,
+      boxOrient: "vertical",
+      display: "-webkit-box",
     },
   },
 });
