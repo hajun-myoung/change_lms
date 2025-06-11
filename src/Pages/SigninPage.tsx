1f529cf17b9cbcab08cfe51654a7fd3f47ce9d60
Denver <audgkwns44@gmail.com>
2025-06-11 16:16:01 +0900

    [Update]SigninPage fully centeralize

--- src/Pages/SigninPage.tsx
+++ src/Pages/SigninPage.tsx
@@ -31,7 +31,7 @@ export default function SigninPage() {
   };
 
   return (
-    <Box sx={{ maxWidth: 400, mx: "auto" }}>
+    <Box className="wrapper centeralize">
       <Typography className="subtitle">높은뜻 정의교회</Typography>
       <Typography className="subtitle">2025 청년부 여름수련회</Typography>
       <Typography className="subtitle">"CHANGE"</Typography>
