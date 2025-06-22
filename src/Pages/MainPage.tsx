91bc35afea9dc193eae28e9b7daf569a151199dc
Denver <audgkwns44@gmail.com>
2025-06-22 10:04:35 +0900

    [Deploy]Version Indicating Update

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -474,7 +474,7 @@ export default function MainPage() {
       >
         <Box sx={{ height: "2.4vw" }}>
           <Typography variant="footerinfo">
-            Ver0.1.1 | Copyright @DENVER |Design assisted by Sandy, Lim. Eunseo,
+            Ver0.2.0 | Copyright @DENVER |Design assisted by Sandy, Lim. Eunseo,
             Choo.
           </Typography>
         </Box>
